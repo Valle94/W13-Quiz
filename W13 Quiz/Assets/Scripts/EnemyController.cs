cf951fcbad6dae020860003f269cb887c3d0666a
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-11-18 15:36:55 -0500

    Finished Quiz
    Added enemy script for shooting and implemented collision between lasers and player/enemies using tags.
    
    Added comments to player and enemy controller scripts.

--- W13 Quiz/Assets/Scripts/EnemyController.cs
+++ W13 Quiz/Assets/Scripts/EnemyController.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Firing")]
+    [SerializeField] GameObject enemyProjectilePrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileLifetime =5f;
+    [SerializeField] float baseFiringRate = 0.2f;
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minimumFiringRate = 0.1f;
+
     void Start()
     {
-        
+        //Start firing coroutine
+        StartCoroutine(FireRepeat());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    //Trigger for when an enemy is hit by a player laser
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "PlayerLaser")
+        {
+            Debug.Log("Hit");
+            Destroy(other.gameObject); //Destroy Player laser
+            Destroy(gameObject); //Destroy Self
+        }
+    }
+
+    // Firing coroutine
+    IEnumerator FireRepeat()
+    {
+        while (true)
+        {
+            //Instantiate laser at enemy position
+            GameObject instance = Instantiate(enemyProjectilePrefab, 
+                                                transform.position, 
+                                                Quaternion.identity);
+
+            //Use GetComponent to determine if the created instance has a rigidbody
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+            //If it does, start moving it downwards
+            if (rb != null)
+            {
+                //Negative transform.up leads to downwards movement
+                rb.velocity = -transform.up * projectileSpeed;
+            }
+
+            //Destroy the instance after a certain amount of time
+            Destroy(instance, projectileLifetime);
+
+            //Calculate a random firing rate to make enemies more dynamic
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance,
+                                                    baseFiringRate + firingRateVariance);
+            timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+
+            //Wait for a number of seconds calculated above before repeating
+            yield return new WaitForSeconds(timeToNextProjectile);
+        }
     }
 }
--- W13 Quiz/Assets/Scripts/PlayerController.cs
+++ W13 Quiz/Assets/Scripts/PlayerController.cs
@@ -25,17 +25,34 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // Calling movement method, and if space is pressed firing method
+        // One press = one laser
         Move();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Fire();
         }
     }
+
+    // Trigger for when the player is hit by an enemy laser
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "EnemyLaser")
+        {
+            Debug.Log("Hit");
+            Destroy(other.gameObject); //Destroy Enemy laser
+            Destroy(gameObject); //Destroy Self
+        }
+    }
     
+    //Movement method
     void Move()
     {
+        //Getting user input as variable
         float x_pos = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //Clamping x position based on camera bounds
         float clampedX = Mathf.Clamp(transform.position.x + x_pos, minBounds.x, maxBounds.x);
+        //Moving positions based on clamped variable
         transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);    
     }
 
@@ -47,18 +64,23 @@ public class PlayerController : MonoBehaviour
         maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
     }
 
+    //Firing Method
     void Fire()
     {
+        //When called, instantiate a player laser at player position
         GameObject instance = Instantiate(playerProjectile, 
                                             transform.position, 
                                             Quaternion.identity);
 
+        //Use GetComponent to determine if the created instance has a rigidbody
         Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+        //If it does, start moving it upwards
         if (rb != null)
         {
             rb.velocity = transform.up * projectileSpeed;
         }
 
+        //Destroy the instance after a certain amount of time
         Destroy(instance, projectileLifetime);
     }
 }
